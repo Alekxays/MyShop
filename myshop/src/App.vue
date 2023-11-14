822232f0491c5b8efca055d221b3080a00dbd769
Alexandre BOISSEL <alexandre.boissel@epitech.digital>
2023-11-14 16:49:46 +0100

    Commit

---
+++ myshop/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="Logo">
+        <RouterLink to="/"><img class="logo" src="./assets/logo.png" alt=""></RouterLink>
+      </div>
+      <div class="Name"><h1 id="title">MyShop</h1></div>
+      <div class="SearchBar">
+        <input type="text" class="searchTerm" placeholder="What are you looking for?">
+      <button type="submit" class="searchButton">
+        <i class="fa fa-search"></i>
+     </button>
+      </div>
+      <div class="Login"><RouterLink to="/login"><button class="btn">Login</button></RouterLink></div>
+  </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ myshop/src/views/AdminView.vue
@@ -0,0 +1,19 @@
+<template>
+  <body>
+    <div id="menu">
+        <h1>Products</h1>
+        <h1>Catégories</h1>
+        <h1>Users</h1>
+    </div>
+  </body>
+</template>
+  
+<style>
+#menu {
+  display: flex;
+}
+#menu > h1 {
+  align-items: center;
+}
+</style>
+  
